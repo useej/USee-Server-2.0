e04197ca85f86a8086f76dc77aadb6824ea977b5
CodingAperoid <xuefengxu333@outlook.com>
2016-08-11 19:00:44 +0800

    增加话题热词

---
+++ src/main/java/com/usee/dao/ColorDao.java
@@ -0,0 +1,6 @@
+package com.usee.dao;
+
+public interface ColorDao {
+
+	public String getColorById(int id);
+}
---
+++ src/main/java/com/usee/dao/impl/RandomNameDaoImpl.java
@@ -0,0 +1,31 @@
+package com.usee.dao.impl;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Service;
+
+import com.usee.dao.RandomNameDao;
+
+@Service
+public class RandomNameDaoImpl implements RandomNameDao {
+	
+	@Resource
+	private SessionFactory sessionFactory;
+	
+	public String getRandomNameById(int id) {
+		String sql = "SELECT nickname from nicknames where id = ?";
+		Query query = sessionFactory.getCurrentSession().createSQLQuery(sql);
+		query.setInteger(0, id);
+		return (String) query.uniqueResult();
+	}
+
+	public int getRandomNameIdByIdRandomName(String randomUserName) {
+		String sql = "SELECT id from nicknames where nickname = ?";
+		Query query = sessionFactory.getCurrentSession().createSQLQuery(sql);
+		query.setString(0, randomUserName);
+		return (Integer) query.uniqueResult();
+	}
+
+}
