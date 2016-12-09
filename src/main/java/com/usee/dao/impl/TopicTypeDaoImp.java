2c123bee418070dbcbf44ff47886119b679d6f80
zstiu <zstiu@foxmail.com>
2016-12-09 11:08:14 +0800

    修正type字段

--- src/main/java/com/usee/dao/impl/TopicTypeDaoImp.java
+++ src/main/java/com/usee/dao/impl/TopicTypeDaoImp.java
@@ -1,5 +1,7 @@
 package com.usee.dao.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.hibernate.Query;
@@ -33,5 +35,27 @@ public class TopicTypeDaoImp implements TopicTypeDao{
 		// TODO Auto-generated method stub
 		
 	}
+	
+	@Override
+	public void delTypeOfTopic(String topicID) {
+		// TODO Auto-generated method stub
+		String hql = "delete TopicType tt where tt.topicID = ?";
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		query.setString(0, topicID);
+
+//		return (query.executeUpdate() > 0);
+	}
+
+	@Override
+	public String getTypeOfTopic(String topicID) {
+		// TODO Auto-generated method stub
+		String hql = "select typeID from TopicType where topicID=?";
+		Query query = sessionFactory.getCurrentSession().createQuery(hql);
+		query.setString(0, topicID);
+		List<String> list = query.list();
+		
+		return list.toString();
+	}
+
 
 }
